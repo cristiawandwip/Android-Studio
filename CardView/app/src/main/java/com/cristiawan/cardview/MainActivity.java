64b37fc9aad3f69a3564c6423993c7a375330524
cristiawandwip <subaza16@gmail.com>
2021-08-02 04:59:38 +0700

    Minggu-4
    Tugas Coding Android Minggu-4

---
+++ CardView/app/src/main/java/com/cristiawan/cardview/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cristiawan.cardview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ IntentActivity/app/src/main/java/com/cristiawan/intentactivity/Penjualan.java
@@ -0,0 +1,14 @@
+package com.cristiawan.intentactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Penjualan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_penjualan);
+    }
+}
